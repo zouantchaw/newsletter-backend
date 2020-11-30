c53a88b74bdc61b2aca1d17cef290d59de2d947e
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-11-30 13:53:33 -0500

    Newsletter controller

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -7,6 +7,30 @@ class NewslettersController < ApplicationController
 
     def show
         newsletter = Newsletter.find_by(id: params[:id])
+        render json: newsletter
     end
+
+    def create 
+        newsletter = Newsletter.create(issue: params[:issue])
+        render json: newsletter
+    end
+    
+    # Do I need to update newsletter? bc the issue is like a timestamp
+    def update
+        newsletter = Newsletter.find_by_id(params[:id])
+        newsletter.issue = params[:issue]
+    end
+
+    # Do I need to destroy the associated data?
+    def destroy
+        newsletter = bike.find_by_id(params[:id])
+        newsletter.logo.destroy
+        newsletter.headline.destroy
+        newsletter.body.destroy
+        newsletter.social.destroy
+        newsletter.destroy
+        render json: newsletter
+    end
+
 end
  
